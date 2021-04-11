a4a6603090167a71ab1636c6685fd0c8aa4c0fab
Ksheekey <76223988+Ksheekey@users.noreply.github.com>
2021-04-10 21:21:24 -0400

    hw portion done

--- Instructions/SQL-Alchemy HW.py
+++ Instructions/SQL-Alchemy HW.py
@@ -90,8 +90,15 @@ def tobs():
 
     return jsonify(all_tobs)
 
+###----------------------------------------------------------------
+##Return a JSON list of the minimum temperature, the average temperature, 
+#   and the max temperature for a given start or start-end range.
+##When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` 
+#   for all dates greater than and equal to the start date.
+
+
 @app.route("/api/v1.0/<start>")
-def kevin(start):
+def start(start):
     session = Session(engine)
     Mdates = session.query(M.date).all()
     MMdates = list(np.ravel(Mdates))
@@ -104,9 +111,9 @@ def kevin(start):
         search_term = date
 
         if search_term == canonicalized:
-            avgtemp = session.query(func.avg(M.tobs)).filter(M.date >= date).all()
-            maxtemp = session.query(func.max(M.tobs)).filter(M.date >= date).all()
-            mintemp = session.query(func.min(M.tobs)).filter(M.date >= date).all()
+            avgtemp = session.query(func.avg(M.tobs)).filter(M.date >= search_term).all()
+            maxtemp = session.query(func.max(M.tobs)).filter(M.date >= search_term).all()
+            mintemp = session.query(func.min(M.tobs)).filter(M.date >= search_term).all()
 
             for temp in avgtemp:
                 avg = temp
@@ -126,58 +133,81 @@ def kevin(start):
             for temp_min in mint:
                 minimum_temp = temp_min
 
-            station_activity = session.query(M.station, func.count(M.station)).group_by(M.station).order_by(func.count(M.station).desc()).limit(1).all()
-            station_act = list(np.ravel(station_activity))
-            station_id = station_act[0]
-                
-                
+            session = Session(engine)
+            date_activity_last = session.query(M.date).order_by((M.date).desc()).limit(1).all()
+            date_act_last = list(np.ravel(date_activity_last))
+            date_id_last = date_act_last[0]
+
             return (
-                    f"In relation to station {station_id} and provided date {start}:"
+                    f"The average temp between {start} and {date_id_last} was {average_temp}"
                     f"<br>"
                     f"<br>"
-                    f"The average temp is {average_temp}"
+                    f"The maximum temp between {start} and {date_id_last} was {maximum_temp}"
                     f"<br>"
                     f"<br>"
-                    f"The maximum temp is {maximum_temp}"
-                    f"<br>"
-                    f"<br>"
-                    f"The minimum temp is {minimum_temp}"
+                    f"The minimum temp between {start} and {date_id_last} was {minimum_temp}"
                     )
 
-    return jsonify("YOU SUCK PAL TRY AGAIN"), 404
+    return jsonify("YOU STINK PAL, TRY AGAIN")
+
     
-    #return jsonify(MMlist)
-
-###----------------------------------------------------------------
-##Return a JSON list of the minimum temperature, the average temperature, 
-#   and the max temperature for a given start or start-end range.
-##When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` 
-#   for all dates greater than and equal to the start date.
-
-
-
-@app.route("/api/v1.0/<start>")
-def start_only(start):
-    session = Session(engine)
-    date_activity_start = session.query(M.date).all()
-    date_act_start = list(np.ravel(date_activity_start))
-
-    for date in date_activity_start:
-        search_term = date
-
-    x = session.query(M.date).filter(M.date >= search_term).all()
-    x_list = list(np.ravel(x))
-    return jsonify(x_list)
-
-    #return jsonify(date_act_start), 404
-
-
 ###----------------------------------------------------------------  
 ##When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` 
 #   for dates between the start and end date inclusive.   
 
-#@app.route("/api/v1.0/<start>/<end>")
-#def start_end():
+@app.route("/api/v1.0/<tart>/<end>")
+def date_tart(tart,end):
+    session = Session(engine)
+    Mdates = session.query(M.date).all()
+    MMdates = list(np.ravel(Mdates))
+    MMlist = []
+    #EElist = []
+    for dates in MMdates:
+        MMlist.append(dates)
+        #EElist.append(dates)
+
+    canonicalized = end
+    for date in MMlist:
+        search_term = date
+
+        if search_term == canonicalized:
+            abc_avgtemp = session.query(func.avg(M.tobs)).filter(M.date <= search_term).all()
+            abc_maxtemp = session.query(func.max(M.tobs)).filter(M.date <= search_term).all()
+            abc_mintemp = session.query(func.min(M.tobs)).filter(M.date <= search_term).all()
+
+            for abc_temp in abc_avgtemp:
+                abc_avg = abc_temp
+
+            for abc_temp_ave in abc_avg:
+                abc_average_temp = abc_temp_ave
+
+            for abc_mxtemp in abc_maxtemp:
+                abc_maxt = abc_mxtemp
+
+            for abc_temp_max in abc_maxt:
+                abc_maximum_temp = abc_temp_max
+                
+            for abc_mntemp in abc_mintemp:
+                abc_mint = abc_mntemp
+
+            for abc_temp_min in abc_mint:
+                abc_minimum_temp = abc_temp_min
+
+
+            return (
+                    f"The average temp between the dates given was {abc_average_temp}"
+                    f"<br>"
+                    f"<br>"
+                    f"The maximum temp between the dates given was {abc_maximum_temp}"
+                    f"<br>"
+                    f"<br>"
+                    f"The minimum temp between the dates given was {abc_minimum_temp}"
+                    )
+
+    return jsonify("YOU STINK PAL, TRY AGAIN"), 404
+ 
+ 
+ 
 
 ###----------------------------------------------------------------
 
@@ -191,8 +221,12 @@ def home():
     date_act_last = list(np.ravel(date_activity_last))
     date_id_first = date_act_first[0]
     date_id_last = date_act_last[0]
+    station_activity = session.query(M.station, func.count(M.station)).group_by(M.station).order_by(func.count(M.station).desc()).limit(1).all()
+    station_act = list(np.ravel(station_activity))
+    station_id = station_act[0]
     return (
         f"Welcome to Climate Analysis and Exploration<br>"
+        f"(earliest date is {date_id_first}, latest date is {date_id_last})<br>"
         "<br>"
         f"for precipitation info type the following onto the end of the task bar:<br>"
         f"/api/v1.0/precipitation <br>"
@@ -200,16 +234,15 @@ def home():
         f"for station info type type the following onto the end of the task bar:<br>"
         f"/api/v1.0/stations <br>"
         "<br>"
-        f"for temperature info type the following onto the end of the task bar:<br>"
+        f"for temperature info for the busiest station ({station_id}) type the following onto the end of the task bar:<br>"
         f"/api/v1.0/tobs <br>"
         "<br>"
-        f"for temperature info starting on a certain date type the following onto the end of the task bar (earliest date is {date_id_first}):<br>"
+        f"for temperature info starting on a certain date type the following onto the end of the task bar then the date requested. (example /api/v1.0/2012-07-12):<br>"
         f" /api/v1.0/<start> <br>"
         "<br>"
-        f"for temperature info between two dates type the following onto the end of the task bar (earliest date is {date_id_first}, latest date is {date_id_last}):<br>"
+        f"for temperature info between two dates type the following onto the end of the task bar then the date requested. (example /api/v1.0/2013-04-29/2014-11-06):<br>"
         f" /api/v1.0/<start>/<end> <br>"
         f"<br>"
-        f" /api/v1.0/kevin"
         f"<br>"
         f"<br>"
         f"<br>"
